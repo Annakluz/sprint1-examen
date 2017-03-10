833963d990d57df1fad8be79d50dda2802e010a4
Anna karina Luz <anna.luz.duran@gmail.com>
2017-03-10 15:40:28 +0000

    versionfinal final

--- js/app.js
+++ js/app.js
@@ -7,40 +7,25 @@ return estudiantes; //Debe regresar el arreglo
 
 }
 
-function agregarEstudiante() {
+function agregarEstudiante(nombre, puntosTecnicos, puntosHse) {
 
-  for(i=0; i<nEstudiantes; i++){ // se que debe ir un for para poder repetir la accion cada que se ponga una estudiante pero no se con que comparar
-      var nEstudiantes = 7; // o puse al azar para poder identificar el error
-       var estudiante = {};// esto deberia en teoria inicarme un arreglo
-        estudiante.nombre = prompt("Ingresa el nombre " );
-        estudiante.puntaje = prompt("Puntaje " );
-        estudiante.puntosHse = prompt("Ingrese los puntos HSE" );
-        estudiantes.push(estudiante); //para acomodarlo en la lista de la variable que nos da el arreglo vacio.
+        var nombre = prompt("ingrese nombre alumna");  //se crea una lista de objetos
+        var puntosTec = prompt("Puntos Tecnicos");
+        var puntosHse = prompt("puntosHse");
+        var estudiante = {
+          "nombre": nombre,
+          "puntosTec": puntosTec,
+          "puntosHse" : puntosHse
+        };
+
+        estudiantes.push(estudiante); //el objeto resultante en este caso estudiante se agrega al arreglo estudiantes.
+        return estudiante;  //regresamos el ultimo estudiante ingresado.
+
     }
 
 
-    return (estudiante); // deberia regresarme el arreglo.
 
-}
 
-    // TO DO: Preguntar al usuario por el nombre, puntos técnicos y puntos de HSE de un estudiante
-    // Hint: El estudiante debe ser agregado a la lista de estudiantes
-    // TO DO: Retornar el estudiante recientemente creado
-    /*PSEUDOCODIGO:
-     “STUDENT RECORDS”
-    FUNCIÓN AGREGAR:
-    1.- Crear una lista de objetos con los siguientes atributos:
-    			Nombre:
-    			Puntos tecnicos:
-    			Puntos HSE:
-
-    		2.Cada uno de los atributos deberán aparecer en un prompt
-    String “Ingrese nombre Alumna”.____
-    Puntos tecnicos:
-    Puntos HSE
-    De manera tal que el valor nombre: sea el resultado del prompt 1
-    3.Guardar la lista en un arreglo variable que sea un arreglo vacío; en este caso estudiantes [];
-    Un for para repetir la accion  un push para acomodarlo al final */
 
 
 
@@ -52,9 +37,9 @@ function mostrar(estudiante) {
     resultado += "<div class='col m12'>";
     resultado += "<div class='card blue-grey darken-1'>";
     resultado += "<div class='card-content white-text'>";
-    resultado += "<p><strong>Nombre:</strong> " + estudiante + "</p>";
-    resultado += "<p><strong>Puntos Técnicos:</strong> " + estudiante + "</p>";
-    resultado += "<p><strong>Puntos HSE:</strong> " + estudiante + "</p>";
+    resultado += "<p><strong>Nombre:</strong> " + estudiante.nombre + "</p>";
+    resultado += "<p><strong>Puntos Técnicos:</strong> " + estudiante.puntosTec + "</p>";
+    resultado += "<p><strong>Puntos HSE:</strong> " + estudiante.puntosHse + "</p>";
     resultado += "</div>";
     resultado += "</div>";
     resultado += "</div>";
@@ -63,24 +48,39 @@ function mostrar(estudiante) {
 }
 
 function mostrarLista(estudiantes) {
-    // TO DO: Iterar la lista del estudiantes para devolverlos en el formato que usa la función mostrar(estudiante)
+  var estudiantesLista = ""; // se crea una variable con un string vacio para despues concatenar en este el resultado.
+    for(i=0; i < estudiantes.length; i++){ //este ciclo sirve para ir mostrando la lista de estudiantes    // TO DO: Iterar la lista del estudiantes para devolverlos en el formato que usa la función mostrar(estudiante)
     // Retornar el template de todos los estudiantes
+      estudiantesLista += mostrar(estudiantes[i]); // con esto mostramos  la lista de estudiantes se concatena para ir llenando el string vacio con cada iteracion.
 
+
+  }
+  return estudiantesLista;
 }
 
 function buscar(nombre, estudiantes) {
+   var busquedaEstudiantes = estudiantes.filter (function (alumna){
+     return alumna.nombre.tiUpperCase == nombre.toUpperCase;
+   });
+
     // TO DO: Buscar el nombre en la lista de estudiantes que se recibe por parámetros
     // Retornar el objeto del estudiante buscado
     // Nota: NO IMPORTA SI EL USUARIO ESCRIBE EL NOMBRE EN MAYÚSCULAS O MINÚSCULAS
 
 }
 
-function topTecnico(estudiantes) {
+function topTecnico(estudiantes) { // hasta este punto funciona ..
+  var ordenartecnico = estudiantes.sort(function(puntosA, puntosB){ //primer objeto contra siguente objeto arreglo
+    return puntosB.puntosTec > puntosA.puntosTec;
+  });
     // TO DO: Retornar el arreglo de estudiantes ordenado por puntaje técnico de mayor a menor
 
 }
 
 function topHSE(estudiantes) {
+    var ordenarHse = estudiantes.sort(function(puntosA, puntosB){
+      return puntosB.puntosHse > puntosA.puntosHse;
+    })
     // TO DO: Retornar el arreglo de estudiantes ordenado por puntaje de HSE de mayor a menor
 
 }
